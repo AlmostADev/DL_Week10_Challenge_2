cac72d5c05cacb9111036c6262751bab765a0a1c
Manuel Olmos <manuelalejandro.soto1@gmail.com>
2018-07-07 13:26:07 -0400

    Some changes on Exercise2 file

--- Ejercicio2.rb
+++ Ejercicio2.rb
@@ -1,24 +1,49 @@
 # Se tienen las clases Rectangulo y Cuadrado cuyos constructores reciben las medidas de los lados correspondientes.
-
 class Rectangulo
+  attr_reader :largo, :ancho
+  # Implementar -mediante Mixin- el módulo en las clases Rectangulo y Cuadrado.
+  include Formula
+
   def initialize(largo, ancho)
     @largo = largo
     @ancho = ancho
   end
-end
 
-class Cuadrado
-  def initialize(lado)
-    @lado = lado
+  # Agregar un método de instancia llámado lados en ambas clases. El método debe imprimir un string con las medidas de los lados.
+  def lados
+    puts "Las medidas de los lados del Rectangulo son #{}"
   end
 end
 
-# Se pide:
+class Cuadrado
+  attr_reader :largo
+  # Implementar -mediante Mixin- el módulo en las clases Rectangulo y Cuadrado.
+  include Formula
 
-# Agregar un método de instancia llámado lados en ambas clases. El método debe imprimir un string con las medidas de los lados.
+  def initialize(lado)
+    @lado = lado
+  end
+
+  # Agregar un método de instancia llámado lados en ambas clases. El método debe imprimir un string con las medidas de los lados.
+  def lados
+    puts "Las medidas de los lados del Cuadrado son #{}"
+  end
+end
+
 # Crear un módulo llamado Formula.
-# Dentro del módulo Formula crear un método llamado perimetro que reciba dos argumentos (lados) y devuelva el perímetro.
-# Dentro del módulo Formula crear un método llamado area que reciba dos argumentos (lados) y devuelva el área.
-# Implementar -mediante Mixin- el módulo en las clases Rectangulo y Cuadrado.
+module Formula
+
+  # Dentro del módulo Formula crear un método llamado perimetro que reciba dos argumentos (lados) y devuelva el perímetro.
+  def Perimetro
+    
+  end
+
+  # Dentro del módulo Formula crear un método llamado area que reciba dos argumentos (lados) y devuelva el área.
+  def Area
+
+  end
+end
+
+
 # Instanciar un Rectangulo y un Cuadrado.
 # Imprimir el área y perímetro de los objetos instanciados utilizando el método del módulo implementado.
