f4c39a0a0d777f1b54dadbf10b3f417651a8cb81
Manuel Olmos <manuelalejandro.soto1@gmail.com>
2018-07-07 13:19:12 -0400

    Exercise1 done

--- Ejercicio1.rb
+++ Ejercicio1.rb
@@ -1,26 +1,28 @@
 class Alumno
-  def initialize(nombre, nota1, nota2, nota3, nota4)
-    @nombre = nombre
-    @nota1 = nota1
-    @nota2 = nota2
-    @nota3 = nota3
-    @nota4 = nota4
-  end
+    attr_reader :nombre, :nota1, :nota2, :nota3, :nota4
+    def initialize(nombre, nota1, nota2, nota3, nota4)
+        @nombre = nombre
+        @nota1 = nota1
+        @nota2 = nota2
+        @nota3 = nota3
+        @nota4 = nota4
+    end
+
+    # Crear un método de clase llamado read_file que reciba como argumento el nombre del archivo
+    # (por defecto debe ser 'notas.txt') y devuelva la colección de objetos. El método debe alojar
+    # las instrucciones que se encuentran después de la clase. Finalmente imprimir la colección de objetos generada.
+    def.selfread(filename)
+        
+        alumnos = []
+        data = []
+
+        File.open(filename, 'r') { |file| data = file.readlines }
+        data.each do |alumno|
+            alumnos << Alumno.new(*alumno.split(', '))
+        end
+
+        print alumnos
+    end
 end
 
-alumnos = []
-data = []
-File.open('notas.txt', 'r') { |file| data = file.readlines }
-data.each do |alumno|
-  alumnos << Alumno.new(*alumno.split(', '))
-end
-
-print alumnos
-
-# Se pide:
-
-# Crear un método de clase llamado read_file que reciba como argumento el nombre del archivo
-# (por defecto debe ser 'notas.txt') y devuelva la colección de objetos. El método debe alojar
-# las instrucciones que se encuentran después de la clase. Finalmente imprimir la colección de objetos generada.
-
-# Hint: Debes reemplazar el argumento de File.open con el nombre del argumento del método read_file.
+Alumno.read_file('notas.txt')
