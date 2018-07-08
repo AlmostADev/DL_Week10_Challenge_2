14ff02c15d2971861b678c1a4e51e3a8b9fa2e5c
Manuel Olmos <manuelalejandro.soto1@gmail.com>
2018-07-08 15:06:07 -0400

    create 404.html file and change to downcase url variable

--- Ejercicio3.rb
+++ Ejercicio3.rb
@@ -5,9 +5,9 @@ require 'rack'
 class MiPrimeraWebApp
   def call(env)
     @url = env['REQUEST_PATH']
-    if @URL == '/INDEX'
+    if @url == '/index'
       [200, { 'Content-Type' => 'text/html' }, ['<h1> Estás en el Index! </h1>']]
-    elsif @URL == '/INDEX2'
+    elsif @url == '/index2'
       [200, { 'Content-Type' => 'text/html' }, ['<h1> Estás en el Landing! </h1>']]
     else
       [404, { 'Content-Type' => 'text/html' }, [File.read("404.html")]]
@@ -16,6 +16,3 @@ class MiPrimeraWebApp
 end
 run MiPrimeraWebApp.new
 
-# Crear un archivo llamado 404.html cuyo body contenga una etiqueta de título con el texto "No se ha encontrado la página :(".
-
-# Modificar el archivo config.ru para adaptarlo a los siguientes requerimientos:
